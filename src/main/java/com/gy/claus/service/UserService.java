ce3956458dd87b50533bf8728d5bb8fb8a28013e
GaoYang <18817393346@163.com>
2018-11-04 19:30:14 +0800

    update
    Signed-off-by: GaoYang <18817393346@163.com>

--- src/main/java/com/gy/claus/service/UserService.java
+++ src/main/java/com/gy/claus/service/UserService.java
@@ -1,6 +1,5 @@
 package com.gy.claus.service;
 
-import com.gy.claus.GyMain;
 import com.gy.claus.Util.GyLog;
 import com.gy.claus.Util.GyLogProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
